eb89ad8313fa511711db47d7ab1e847746212512
Sam Lansley <Sam.Lansley@traverssmith.com>
2023-05-10 15:38:22 +0100

    removed edit button from DatasetDataSideBar

--- YCNRefine/ClientApp/src/components/DatasetDataSidebar/components/Item/Item.js
+++ YCNRefine/ClientApp/src/components/DatasetDataSidebar/components/Item/Item.js
@@ -2,8 +2,6 @@ import React from "react";
 import classes from "./Item.module.css";
 import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
 import { Link } from "react-router-dom";
-import Edit from '@mui/icons-material/Edit';
-import { IconButton } from "@mui/material";
 
 const Item = ({ data, itemUrl }) => {
     const {
@@ -12,22 +10,12 @@ const Item = ({ data, itemUrl }) => {
     } = data;
 
     return (
-        <div className={classes.item}>
-            <Link to={itemUrl + id} className={classes.itemLink}>
-                <ChatBubbleIcon
-                    fontSize="small"
-                />
-                <div>{name}</div>
-            </Link>
-            <div className={classes.rightSide}>
-                <IconButton>
-                    <Edit
-                        fontSize="small"
-                    />
-                </IconButton>
-            </div>
-        
-        </div>
+        <Link to={itemUrl + id} className={classes.itemLink}>
+            <ChatBubbleIcon
+                fontSize="small"
+            />
+            <div>{name}</div>
+        </Link>
     )
 };
 
